ed021be04f5a06ce185c614f07266f41cbc5077e
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2018-05-04 16:12:00 +0200

    Added sleep to lcd prototype before exiting so one can read the message on the lcd screen

--- LCD/lcd.c
+++ LCD/lcd.c
@@ -1,5 +1,6 @@
 #include <wiringPi.h>
 #include <lcd.h>
+#include <unistd.h>
 
 #define LCD_RS 25 //Register select pin
 #define LCD_E 24 //Enable pin
@@ -14,4 +15,7 @@ int main()
 	wiringPiSetup();
 	lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
 	lcdPuts(lcd, "OCT Scanner");
+
+	usleep(5000000);
+	return 0;
 }
