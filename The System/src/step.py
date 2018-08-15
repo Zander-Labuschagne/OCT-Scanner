536d4a6617686e0a37112a4e5e8bb19a3f8fd69f
Zander-Labuschagne <Zander.Labuschagne@protonmail.ch>
2018-08-15 15:37:42 +0200

    Using the System or exec function instead of IPC, for now...

--- The System/src/step.py
+++ The System/src/step.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import posix_ipc
 
- 
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -12,7 +12,7 @@ IN3 = 23 # blue
 IN4 = 24 # yellow
 
 QUEUE_KEY = 8500
- 
+
 step_count = 8
 seq = list(range(0, StepCount))
 seq[0] = [1,0,0,0]
@@ -23,18 +23,18 @@ seq[4] = [0,0,1,0]
 seq[5] = [0,0,1,1]
 seq[6] = [0,0,0,1]
 seq[7] = [1,0,0,1]
- 
+
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
-  
+
 def set_step(w1, w2, w3, w4):
 	GPIO.output(IN1, w1)
 	GPIO.output(IN2, w2)
 	GPIO.output(IN3, w3)
 	GPIO.output(IN4, w4)
- 
+
 #Die funksie is vir die IPC metode wat dan deur volle omwenteling hardloop en kommunikeer met C++
 def rotate(scan_resolution):
 	for i in range(scan_resolution):
@@ -43,7 +43,7 @@ def rotate(scan_resolution):
 			time.sleep(0.005)
 
 #Die funksie is vir die Shell exec metode wat net step elke keer as hy geroep word
-def step():
+def step(step_count):
 	for ii in range(step_count):
 		set_step(seq[ii][0], seq[ii][1], seq[ii][2], seq[ii][3])
 		time.sleep(0.005)
@@ -58,4 +58,4 @@ if __name__ == '__main__':
 	# 	print "ERROR: message queue creation failed"
 
 	# mq.send(“rotated”, True)
-	step()
+	step(8)
